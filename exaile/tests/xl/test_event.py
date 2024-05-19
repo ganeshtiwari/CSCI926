8ec983dce61106a16963721c7def8b89df51d79d
ruwinisr <163369631+ruwinisr@users.noreply.github.com>
2024-05-19 12:45:03 +1000

    Update test_event.py

--- exaile/tests/xl/test_event.py
+++ exaile/tests/xl/test_event.py
@@ -1,98 +1,151 @@
-# from gi.repository import GLib
-# import threading
-# from xl import event
+import pytest
+import weakref
+import types
+import gc
+from unittest.mock import MagicMock, ANY, call, patch
+from xl.event import log_event, add_callback, add_ui_callback, remove_callback, EVENT_MANAGER, Event, Callback, _WeakMethod, _NONE, EventManager
 
-# # nasty globals
-# on_ui_thread = [False]
-# calls = [0]
+@pytest.fixture
+def event_manager_mock(monkeypatch):
+    mock = MagicMock()
+    monkeypatch.setattr("xl.event.EVENT_MANAGER", mock)
+    return mock
 
+def test_log_event(event_manager_mock):
+    log_event('test_event', 'sender_object', 'event_data')
+    assert event_manager_mock.emit.called
+    event_arg = event_manager_mock.emit.call_args[0][0]
+    assert isinstance(event_arg, Event)
+    assert hasattr(event_arg, 'type') and event_arg.type == 'test_event'
+    assert hasattr(event_arg, 'object') and event_arg.object == 'sender_object'
+    assert hasattr(event_arg, 'data') and event_arg.data == 'event_data'
 
-# # TODO: monkeypatch instead
-# def glib_idle_add(fn, *args):
-#     was_ui_thread = on_ui_thread[0]
-#     on_ui_thread[0] = True
+def test_add_callback(event_manager_mock):
+    callback = lambda x: x
+    add_callback(callback, 'event_type', 'event_object')
+    event_manager_mock.add_callback.assert_called_once_with(
+        callback, 'event_type', 'event_object', (), {}
+    )
 
-#     try:
-#         fn(*args)
-#     finally:
-#         on_ui_thread[0] = was_ui_thread
+def test_add_ui_callback(event_manager_mock):
+    callback = lambda x: x
+    add_ui_callback(callback, 'ui_event_type', 'ui_event_object')
+    event_manager_mock.add_callback.assert_called_once_with(
+        callback, 'ui_event_type', 'ui_event_object', (), {}, ui=True
+    )
 
+def test_remove_callback(event_manager_mock):
+    callback = lambda x: x
+    remove_callback(callback, 'event_type', 'event_object')
+    event_manager_mock.remove_callback.assert_called_once_with(
+        callback, 'event_type', 'event_object'
+    )
 
-# GLib.idle_add = glib_idle_add
+def _getWeakRef(func):
+    return weakref.ref(func)
 
+def test_callback_init():
+    def test_func():
+        return "Testing"
+    args = (1, 2)
+    kwargs = {'a': 3, 'b': 4}
+    callback_time = 1234567890
+    callback = Callback(test_func, callback_time, args, kwargs)
+    assert callback.wfunction() is test_func
+    assert callback.time == callback_time
+    assert callback.args == args
+    assert callback.kwargs == kwargs
 
-# class NormalCallback:
-#     def __init__(self):
-#         self.called = False
-#         event.add_callback(self.on_cb, 'test')
+def test_callback_repr():
+    def test_func():
+        return "Testing"
+    callback_time = 1234567890
+    callback = Callback(test_func, callback_time, (), {})
+    func_repr = repr(callback.wfunction())
+    expected_repr = f"<Callback {func_repr}>"
+    assert repr(callback) == expected_repr
 
-#     def destroy(self):
-#         event.remove_callback(self.on_cb, 'test')
+def test_callback_weakref():
+    def test_func():
+        return "Testing"
+    callback = Callback(test_func, 1234567890, (), {})
+    del test_func
+    assert callback.wfunction() is None
 
-#     def on_cb(self, type, obj, data):
-#         self.called = True
-#         self.on_ui_thread = on_ui_thread[0]
+class SomeClass:
+    def __init__(self, value):
+        self.value = value
 
+    def method(self):
+        return self.value
 
-# class UiCallback:
-#     def __init__(self):
-#         self.called = False
-#         event.add_ui_callback(self.on_cb, 'test')
+def test_weakMethod_init_call():
+    obj = SomeClass(10)
+    method = obj.method
+    weak_method = _WeakMethod(method)
+    assert weak_method()() == 10
 
-#     def destroy(self):
-#         event.remove_callback(self.on_cb, 'test')
+def test_weakMethod_equality():
+    obj1 = SomeClass(10)
+    obj2 = SomeClass(20)
+    weak_method1 = _WeakMethod(obj1.method)
+    weak_method2 = _WeakMethod(obj1.method)
+    weak_method3 = _WeakMethod(obj2.method)
+    assert weak_method1 == weak_method2
+    assert weak_method1 != weak_method3
 
-#     def on_cb(self, type, obj, data):
-#         if on_ui_thread[0]:
-#             self.called = True
+def test_weakMethod_hash():
+    obj = SomeClass(10)
+    weak_method = _WeakMethod(obj.method)
+    assert hash(weak_method) == hash(obj.method.__func__)
 
+def test_weakMethod_repr():
+    obj = SomeClass(10)
+    weak_method = _WeakMethod(obj.method)
+    assert '; DEAD' not in repr(weak_method)
+    del obj
+    assert '; DEAD' in repr(weak_method)
 
-# def _init_events():
-#     event.EVENT_MANAGER = event.EventManager()
+def test_weakMethod_refs():
+    obj = SomeClass(10)
+    method = obj.method
+    weak_method = _WeakMethod(method)
+    obj_ref = weakref.ref(obj)
+    assert weak_method.refs(obj_ref)
 
+@pytest.fixture
+def event_manager():
+    return EventManager(use_logger=True, logger_filter='test', verbose=True)
 
-# def _finish_events():
-#     assert len(event.EVENT_MANAGER.callbacks) == 0
-#     assert len(event.EVENT_MANAGER.all_callbacks) == 0
-#     assert len(event.EVENT_MANAGER.ui_callbacks) == 0
+class TestObject:
+    pass
 
+def test_add_remove_callback(event_manager):
+    callback = MagicMock()
+    evty = 'test_event'
+    obj = TestObject()
+    remove = event_manager.add_callback(callback, evty, obj, (), {})
+    assert evty in event_manager.callbacks
+    assert obj in event_manager.callbacks[evty]
+    assert event_manager.callbacks[evty][obj][0].wfunction() == callback
+    remove()
+    assert evty not in event_manager.callbacks or obj not in event_manager.callbacks[evty]
 
-# def test_ui_events():
-#     _init_events()
-#     ucb = UiCallback()
-#     ncb = NormalCallback()
+def test_emit(event_manager):
+    event = Event('test_event', TestObject(), 'test_data')
+    callback = MagicMock()
+    event_manager.add_callback(callback, event.type, event.object, (), {})
+    event_manager.emit(event)
+    callback.assert_called_once_with(event.type, event.object, event.data)
 
-#     on_ui_thread[0] = True
-#     event.log_event('test', ucb, None)
+def test_emit_async(event_manager):
+    event = Event('test_event', TestObject(), 'test_data')
+    callback = MagicMock()
+    event_manager.add_callback(callback, event.type, event.object, (), {})
+    with patch('xl.event.GLib.idle_add', side_effect=lambda f, *args, **kwargs: f(*args, **kwargs)):
+        event_manager.emit_async(event)
+    callback.assert_called_once_with(event.type, event.object, event.data)
 
-#     assert ncb.called is True
-#     assert ncb.on_ui_thread is True
-#     assert ucb.called is True
-
-#     ucb.destroy()
-#     ncb.destroy()
-
-#     _finish_events()
-
-
-# def test_thread_events():
-#     _init_events()
-#     ucb = UiCallback()
-#     ncb = NormalCallback()
-
-#     def _run():
-#         on_ui_thread[0] = False
-#         event.log_event('test', ucb, None)
-
-#     t = threading.Thread(target=_run)
-#     t.start()
-#     t.join()
-
-#     assert ncb.called is True
-#     assert ncb.on_ui_thread is False
-#     assert ucb.called is True
-
-#     ucb.destroy()
-#     ncb.destroy()
-
-#     _finish_events()
+def test_emit_no_callbacks(event_manager):
+    event = Event('test_event', 'test_object', 'test_data')
+    event_manager.emit(event)
